fd7f2f0fd202d9ce23ece0e27976cbb1bedf8d05
reverendus <reverendus@invalid>
2019-03-02 22:18:58 +0100

    Unit-tested for a use case with two addresses

--- tests/test_petrometer.py
+++ tests/test_petrometer.py
@@ -58,6 +58,12 @@ class TestPetrometer:
         mock.get("https://api.etherscan.io/api?module=account&action=txlist&address=0x52a043195a2803cc7e75f17f5c9d4f84ffa33211&startblock=4981201&endblock=99999999&page=1&offset=100&sort=asc&apikey=SOMEKEY",
                  text=datadir.join('startblock_4981201.json').read_text('utf-8'))
 
+        mock.get("https://api.etherscan.io/api?module=account&action=txlist&address=0x9041fe5b3fdea0f5e4afdc17e75180738d877a01&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey=SOMEKEY",
+                 text=datadir.join('addr2_startblock_0.json').read_text('utf-8'))
+
+        mock.get("https://api.etherscan.io/api?module=account&action=txlist&address=0x9041fe5b3fdea0f5e4afdc17e75180738d877a01&startblock=4981201&endblock=99999999&page=1&offset=100&sort=asc&apikey=SOMEKEY",
+                 text=datadir.join('addr2_startblock_4981201.json').read_text('utf-8'))
+
     def test_should_print_usage_when_no_arguments(self):
         # when
         with captured_output() as (out, err):
@@ -128,5 +134,44 @@ Gas usage summary for: 0x52a043195a2803cc7e75f17f5c9d4f84ffa33211
 
 Number of transactions: 71
 Total gas cost: 0.64726249 ETH ($543.27)
+
+"""
+
+    def test_with_two_addresses(self, datadir):
+        # remove local cache file
+        try:
+            db_folder = user_cache_dir("petrometer", "maker")
+            db_file = os.path.join(db_folder, "0x52a043195a2803cc7e75f17f5c9d4f84ffa33211.txdb")
+            db_file = os.path.join(db_folder, "0x9041fe5b3fdea0f5e4afdc17e75180738d877a01.txdb")
+            os.remove(db_file)
+        except:
+            pass
+
+        # run `petrometer`
+        with captured_output() as (out, err):
+            # when
+            with requests_mock.Mocker(real_http=True) as mock:
+                self.mock_api(mock, datadir)
+                Petrometer(args(f"--etherscan-api-key SOMEKEY"
+                                f" 0x52a043195a2803cc7e75f17f5c9d4f84ffa33211"
+                                f" 0x9041fe5b3fdea0f5e4afdc17e75180738d877a01")).main()
+
+            # then
+            assert out.getvalue() == f"""
+Gas usage summary for: 0x52a043195a2803cc7e75f17f5c9d4f84ffa33211
+                       0x9041fe5b3fdea0f5e4afdc17e75180738d877a01
+
+    Day         All tx     Failed tx      (%)          Average gas price         Average tx cost        ($)         Total tx cost           ($)     
+====================================================================================================================================================
+2017-12-29            16            0     (0.0%)                    3.6 GWei         0.00972251 ETH    ($7.19)         0.15556020 ETH      ($115.05)
+2017-12-30            21            0     (0.0%)                    2.5 GWei         0.00953677 ETH    ($6.61)         0.20027212 ETH      ($138.79)
+2017-12-31            10            0     (0.0%)                    3.9 GWei         0.00703955 ETH    ($5.22)         0.07039553 ETH       ($52.17)
+2018-01-01             2            0     (0.0%)                    2.1 GWei         0.00413893 ETH    ($3.13)         0.00827785 ETH        ($6.26)
+2018-01-03             1            0     (0.0%)                   20.0 GWei         0.00061472 ETH    ($0.58)         0.00061472 ETH        ($0.58)
+2018-01-05             1            0     (0.0%)                   40.0 GWei         0.03426612 ETH   ($33.14)         0.03426612 ETH       ($33.14)
+2018-01-27            23            1     (4.3%)                    3.5 GWei         0.00879653 ETH    ($9.76)         0.20232028 ETH      ($224.39)
+
+Number of transactions: 74
+Total gas cost: 0.67170682 ETH ($570.38)
 
 """
